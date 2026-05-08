2d950b66eda730d4cffc3968791b5adfd7216ffb
Kyle Wardlow <8558723+kylewardlow@users.noreply.github.com>
2026-05-08 16:20:36 -0400

    feat: Add example operation of VEAB control

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 """Hello."""
 
 import json
-import socket
 from os import getenv
 from pathlib import Path
+import logging
+from pgva import PGVA, PGVATCPConfig
 
 from applied_motion import Gantry
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s.%(msecs)03d | %(levelname)-8s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[logging.FileHandler("applied_motion.log"), logging.StreamHandler()],
+)
 
 _DEFAULT_FPOSAPI_IP = "192.168.10.25"
 _DEFAULT_FPOSAPI_PORT = 1234
@@ -18,6 +26,14 @@ with fixture_path.open() as fh:
     cfg["connection"]["ip"] = ip
     cfg["connection"]["port"] = port
 
-
+pgva = PGVA(config=PGVATCPConfig(interface="tcp/ip", ip="192.168.10.102"))
+pgva.set_output_pressure(449)
 gantry = Gantry.from_config(cfg)
-gantry.home()
+gantry._client.get_veab()
+gantry._client.set_veab(200)
+import time
+
+gantry._client.get_veab()
+
+time.sleep(1)
+gantry._client.get_veab()
